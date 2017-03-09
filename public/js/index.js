2f17ca17981860c5aee54dbeeac5aa2693ad3553
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-09 18:32:18 +0100

    changed <p> in index.js into </p>

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,13 @@ $.getJSON("https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad68457
     var p2   = "<p>" + 'Week number: ' + val.season_week + "</p>";
     var p3   = "<p class='days-button'>" + 'Celebrations: ' + '(click to expand)' + "</p>";
     var div2 = "<div class='days' id='celebrations'>";
-    var p11  = "<p>" + 'Title: ' + val.celebrations[0].title  + "<p>";
-    var p12  = "<p>" + 'Colour: ' + val.celebrations[0].colour + "<p>";
-    var p13  = "<p>" + 'Rank: ' + val.celebrations[0].rank + "<p>";
-    var p14  = "<p>" + 'Rank number: ' + val.celebrations[0].rank_num + "<p></div>";
+    var p11  = "<p>" + 'Title: ' + val.celebrations[0].title  + "</p>";
+    var p12  = "<p>" + 'Colour: ' + val.celebrations[0].colour + "</p>";
+    var p13  = "<p>" + 'Rank: ' + val.celebrations[0].rank + "</p>";
+    var p14  = "<p>" + 'Rank number: ' + val.celebrations[0].rank_num + "</p></div>";
     var p4   = "<p>" + 'Week day: ' + val.weekday + "</p><div></button></div>";
+
+
 
     var group = h5+div1+p1+p2+p3+div2+p11+p12+p13+p14+p4;
 
