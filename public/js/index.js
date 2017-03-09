6d02344354c818d68e009b32d84021d38075f33e
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-09 16:01:08 +0100

    added styling and search bar

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,29 @@
-$.getJSON( "https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad684579e70123f253ea20d0e7befc/data.json", function(data) {
+$.getJSON("https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad684579e70123f253ea20d0e7befc/data.json", function(data) {
 
   $.each(data, function(key, val) {
 
-    var h5   = "<button class='btn btn-outline-primary' type='button'><h5 class='days-button'>" + data[key].date + "</h5>";
+    var h5   = "<button class='btn btn-outline-primary' type='button'><h5 class='days-button'>" + val.date + "</h5>";
     var div1 = "<div class='days'>";
-    var p1   = "<p>" + 'Season: ' + data[key].season + "</p>";
-    var p2   = "<p>" + 'Week number: ' + data[key].season_week + "</p>";
+    var p1   = "<p>" + 'Season: ' + val.season + "</p>";
+    var p2   = "<p>" + 'Week number: ' + val.season_week + "</p>";
     var p3   = "<p class='days-button'>" + 'Celebrations: ' + '(click to expand)' + "</p>";
     var div2 = "<div class='days' id='celebrations'>";
-    var p11  = "<p>" + 'Title: ' + data[key].celebrations[0].title  + "<p>";
-    var p12  = "<p>" + 'Colour: ' + data[key].celebrations[0].colour + "<p>";
-    var p13  = "<p>" + 'Rank: ' + data[key].celebrations[0].rank + "<p>";
-    var p14  = "<p>" + 'Rank number: ' + data[key].celebrations[0].rank_num + "<p></div>";
-    var p4   = "<p>" + 'Week day: ' + data[key].weekday + "</p><div></button></div>";
+    var p11  = "<p>" + 'Title: ' + val.celebrations[0].title  + "<p>";
+    var p12  = "<p>" + 'Colour: ' + val.celebrations[0].colour + "<p>";
+    var p13  = "<p>" + 'Rank: ' + val.celebrations[0].rank + "<p>";
+    var p14  = "<p>" + 'Rank number: ' + val.celebrations[0].rank_num + "<p></div>";
+    var p4   = "<p>" + 'Week day: ' + val.weekday + "</p><div></button></div>";
 
-    $("#dates").append(h5+div1+p1+p2+p3+div2+p11+p12+p13+p14+p4);
+    var group = h5+div1+p1+p2+p3+div2+p11+p12+p13+p14+p4;
+
+    $("#dates").append(group);
 
   });
     var divs = $("button");
       for(var i = 0; i < divs.length; i += 7) {
         divs.slice(i, i+7).wrapAll("<div class='btn-group'></div>");
       }
+
 });
 
 function main() {
@@ -33,3 +36,21 @@ function main() {
 }
 
 $(document).ready(main);
+
+$(document).ready(function(){
+  $("#filter").keyup(function(){
+    var filter = $(this).val(), count = 0;
+
+    $("button").each(function(){
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+          $(this).fadeOut();
+      } else {
+          $(this).show();
+          count++;
+      }
+    });
+
+    var numberItems = count;
+    $("#filter-count").text("Number of Comments = "+count);
+  });
+});
