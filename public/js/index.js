83b5b187acbe82e2cc5dd496b13e3d1b02e9b39d
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-10 09:14:41 +0100

    added celebration array to days div

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,24 @@
 $.getJSON("https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad684579e70123f253ea20d0e7befc/data.json", function(data) {
 
   $.each(data, function(key, val) {
-
+    console.log(p11);
     var h5   = "<button class='btn btn-outline-primary' type='button'><h5 class='days-button'>" + val.date + "</h5>";
     var div1 = "<div class='days'>";
     var p1   = "<p>" + 'Season: ' + val.season + "</p>" +
                "<p>" + 'Week number: ' + val.season_week + "</p>" +
                "<p class='days-button'>" + 'Celebrations: ' + '(click to expand)' + "</p>";
     var div2 = "<div class='days' id='celebrations'>";
-    var p11  = "<p>" + 'Title: ' + val.celebrations[0].title  + "</p>" +
-               "<p>" + 'Colour: ' + val.celebrations[0].colour + "</p>" +
-               "<p>" + 'Rank: ' + val.celebrations[0].rank + "</p>" +
-               "<p>" + 'Rank number: ' + val.celebrations[0].rank_num + "</p></div>";
-    var p2   = "<p>" + 'Week day: ' + val.weekday + "</p><div></button></div>";
+
+    var p11  =  $.each(val.celebrations, function bla(key2, val2, ddd) {
+                  var obj=
+                   "<p>" + 'Title: ' + val2.title  + "</p>" +
+                   "<p>" + 'Colour: ' + val2.colour + "</p>" +
+                   "<p>" + 'Rank: ' + val2.rank + "</p>" +
+                   "<p>" + 'Rank number: ' + val2.rank_num + "</p>";
+                   ddd(obj);
+
+                });
+    var p2   = "</div><p>" + 'Week day: ' + val.weekday + "</p><div></button></div>";
 
     var group = h5+div1+p1+div2+p11+p2;
 
@@ -32,7 +38,7 @@ $(document).ready(function main() {
     $(this).next().slideToggle(100);
     $(this).toggleClass('active');
 	});
-  
+
   $("#filter").keyup(function(){
     var filter = $(this).val(), count = 0;
 
