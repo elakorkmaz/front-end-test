e377fa3db9f5d2b0c8e17d58f17da2d373c385d1
Ela Korkmaz <elakorkmaz87@gmail.com>
2017-03-10 09:54:07 +0100

    getting each celebration in each date

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 $.getJSON("https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad684579e70123f253ea20d0e7befc/data.json", function(data) {
 
   $.each(data, function(key, val) {
-    console.log(p11);
+    console.log(val.data);
     var h5   = "<button class='btn btn-outline-primary' type='button'><h5 class='days-button'>" + val.date + "</h5>";
     var div1 = "<div class='days'>";
     var p1   = "<p>" + 'Season: ' + val.season + "</p>" +
@@ -9,18 +9,16 @@ $.getJSON("https:bitbucket.org/nadjib-l1nda/front-end-test/raw/30ffd45f11ad68457
                "<p class='days-button'>" + 'Celebrations: ' + '(click to expand)' + "</p>";
     var div2 = "<div class='days' id='celebrations'>";
 
-    var p11  =  $.each(val.celebrations, function bla(key2, val2, ddd) {
-                  var obj=
+    var h11 =   $.each(val.celebrations, function(key2, val2) {
                    "<p>" + 'Title: ' + val2.title  + "</p>" +
                    "<p>" + 'Colour: ' + val2.colour + "</p>" +
                    "<p>" + 'Rank: ' + val2.rank + "</p>" +
-                   "<p>" + 'Rank number: ' + val2.rank_num + "</p>";
-                   ddd(obj);
-
+                   "<p>" + 'Rank number: ' + val2.rank_num + "</p>"
                 });
+
     var p2   = "</div><p>" + 'Week day: ' + val.weekday + "</p><div></button></div>";
 
-    var group = h5+div1+p1+div2+p11+p2;
+    var group = h5+div1+p1+div2+h11+p2;
 
     $("#dates").append(group);
 
